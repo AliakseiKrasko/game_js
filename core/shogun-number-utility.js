bafca375f03e76d075ba50436a00e235a416b5d1
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-19 21:45:26 +0100

    write error checking

--- core/shogun-number-utility.js
+++ core/shogun-number-utility.js
@@ -1,5 +1,13 @@
 export class ShogunNumberUtility {
     getRandomIntegerNumber (fromInclusive, toExlusive) {
+        if (typeof fromInclusive !== 'number' || typeof toExclusive !== 'number') {
+            throw new TypeError('Both arguments must be numbers.');
+        }
+        if (fromInclusive >= toExclusive) {
+            throw new RangeError('fromInclusive must be less than toExclusive.');
+        }
 
+        return Math.floor(Math.random() * (toExclusive - fromInclusive)) + fromInclusive;
+    }
     }
 }
