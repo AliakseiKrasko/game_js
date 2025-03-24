b5f4cc44059befd3e0e2edd90fb3fcadd49f9048
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-24 16:52:59 +0100

    create googleJumpInterval and makeGoogleJump

--- core/game.js
+++ core/game.js
@@ -5,8 +5,17 @@ export class Game {
     #status = GameStatuses.SETTINGS
 
     #gooslePosition = null
+    /**
+     * @private
+     * @type {ShogunNumberUtility}
+     * Утилита для работы со случайными числами.
+     */
     #numberUtility;// = new ShogunNumberUtility()
-
+    /**
+     * Создаёт экземпляр игры с переданной утилитой случайных чисел.
+     *
+     * @param {ShogunNumberUtility} somethingSimilarToNumberUtility - Объект, содержащий метод генерации случайных чисел.
+     */
     constructor(somethingSimilarToNumberUtility) {
         this.#numberUtility = somethingSimilarToNumberUtility
     }
@@ -18,16 +27,45 @@ export class Game {
         },
         googleJumpInterval: 1000
     }
+    /**
+     * Устанавливает новый интервал прыжка для "гугла".
+     *
+     * @param {number} newValue - Новый интервал в миллисекундах. Должен быть положительным числом.
+     * @throws {TypeError} Если передано не число или число меньше/равно нулю.
+     */
+
+    set googleJumpInterval(newValue) {
+        if (typeof newValue !== 'number') {
+            throw new TypeError('Interval must be a number.');
+        }
+        if (newValue <= 0) {
+            throw new TypeError('Interval must be greater than 0.');
+        }
+        this.#settings.googleJumpInterval = newValue;
+    }
 
     start() {
         if (this.#status !== GameStatuses.SETTINGS) {
             throw new Error("Game must be in Settings before Start")
         }
         this.#status = GameStatuses.IN_PROGRESS
-        this.#gooslePosition = {
+        this.#makeGoogleJump()
+
+        setInterval(() => {
+            this.#makeGoogleJump()
+        }, this.#settings.googleJumpInterval )
+    }
+
+    #makeGoogleJump() {
+        const newPosition = {
             x: this.#numberUtility.getRandomIntegerNumber(0, this.#settings.gridSize.columnsCount),
             y: this.#numberUtility.getRandomIntegerNumber(0, this.#settings.gridSize.rowsCount),
         }
+        if(newPosition.x === this.gooslePosition?.x && newPosition.y === this.gooslePosition.y) {
+            this.#makeGoogleJump()
+            return
+        }
+        this.#gooslePosition = newPosition
     }
 
     get status() {
--- core/game.test.js
+++ core/game.test.js
@@ -9,18 +9,39 @@ describe('game', () => {
         expect(game.status).toBe(GameStatuses.SETTINGS)
     })
     it('game should be created and return status', async () => {
-        const game = new Game()
+        const numberUtil = new ShogunNumberUtility()
+        const game = new Game(numberUtil)
         await game.start()
         expect(game.status).toBe(GameStatuses.IN_PROGRESS)
     })
 
     it('google should be in the Grid after start', async ()  => {
-        const game = new Game()
-        expect(game.gooslePosition).toBeNull()
-        await game.start()
-        expect(game.gooslePosition.x).toBeLessThan(game.gridSize.columnsCount)
-        expect(game.gooslePosition.x).toBeGreaterThanOrEqual(0)
-        expect(game.gooslePosition.y).toBeLessThan(game.gridSize.rowsCount)
-        expect(game.gooslePosition.y).toBeGreaterThanOrEqual(0)
+        const numberUtil = new ShogunNumberUtility()
+        for(let i = 0; i < 100; i++){
+            const game = new Game(numberUtil)
+            expect(game.gooslePosition).toBeNull()
+            await game.start()
+            expect(game.gooslePosition.x).toBeLessThan(game.gridSize.columnsCount)
+            expect(game.gooslePosition.x).toBeGreaterThanOrEqual(0)
+            expect(game.gooslePosition.y).toBeLessThan(game.gridSize.rowsCount)
+            expect(game.gooslePosition.y).toBeGreaterThanOrEqual(0)
+        }
+
     })
+
+    it('google should be in the Gridafter but in new position after jump', async () => {
+        const numberUtil = new ShogunNumberUtility()
+        const game = new Game(numberUtil)
+        game.googleJumpInterval = 1;
+        await game.start()
+
+        for(let i = 0; i <= 100; i++){
+            const prevGooglePosition = game.gooslePosition
+            await delay(1)
+            const currentGooglePosition = game.gooslePosition
+            expect(prevGooglePosition).not.toEqual(currentGooglePosition)
+        }
+    } )
 });
+
+const delay = (ms) => new Promise(res =>setTimeout(res, ms))
